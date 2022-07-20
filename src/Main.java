ec48e74a6b8329fc0b7fbd05a8087accb74c8469
Octavio Aranguren <tavoje@gmail.com>
2022-07-20 11:30:26 -0300

    subiendo tarea de OB

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+       suma(12, 20, 15 );
+            }
+    public static void suma(int a, int b, int c) {
+        System.out.println(a+b+c);
+    }
+}
